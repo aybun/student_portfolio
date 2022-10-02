3382fa6e6c0229af3ab5db14c8522e48ef5c7d26
chaleelaleela <aiyah.bt@kkumail.com>
2022-10-02 21:46:51 +0700

    Added multiple skill types.

--- student_portfolio/static/profile/charts.js
+++ student_portfolio/static/profile/charts.js
@@ -8,7 +8,7 @@ let chartComponent = {
             events:[],
             user:{},
 
-            modalTitle:"",
+            modalTitle:"Edit goal points",
             addingNewEvent : false,
 
             // id:0,
@@ -32,10 +32,13 @@ let chartComponent = {
             skillTable:"",
             skillFreq:[],
 
-            radarChart:{},
+            radarCharts:[],
 
             checkboxes: [],
+
+            editingSkillType:0,
             // PhotoFileName:"anonymous.png",
+
             PhotoPath:variables.PHOTO_URL
         }
     },
@@ -49,217 +52,27 @@ let chartComponent = {
             this.event = this.getEmptyEvent()
 
         },
-        addClick(){
 
-            this.modalTitle="Add Event"
-            this.addingNewEvent= true // Signal that we are adding a new event -> Create Button.
-            // document.getElementById("createButton").disabled = false;
-
-            this.event = this.getEmptyEvent()
-
-            this.checkboxes=[]
-            // this.DateOfJoining="",
-            // this.PhotoFileName="anonymous.png"
+        editGoalPoints(skillType){
+            this.editingSkillType = skillType
         },
-        editClick(event){
-            this.modalTitle="Edit Event";
-            this.addingNewEvent = false
-
-            this.event = event
-            this.checkboxes = []
-
-
-            //Consider create a list of checkbox variables.
-            if (event.approved)
-                this.checkboxes.push('approved')
-            if (event.used_for_calculation)
-                this.checkboxes.push('used_for_calculation')
-
-        },
-        createClick(){
-            let before_altered_event = this.getEmptyEvent()
-
-            this.event.skills = this.cleanSkills(this.event.skills);
-            before_altered_event.skills = this.event.skills
-
-            this.event.skills = JSON.stringify(this.event.skills)
-
-            delete this.event['eventId']
-            if (this.event.attachment_file == null || typeof this.event.attachment_file === 'string' || this.event.attachment_file === '' )
-                delete this.event.attachment_file
-
-            let outDict = new FormData();
-            console.log(this.event)
-            for (const [key, value] of Object.entries(this.event)) {
-                // console.log(key, value);
-                outDict.append(key.toString(), value)
-            }
-
-            alert(JSON.stringify(outDict, null, 2))
-
-            // axios.post(variables.API_URL+"event", outDict,
-            //     { headers: {'Content-Type': 'multipart/form-data'}})
-            //     .then((response)=>{
-            //     this.refreshData();
-            //     alert(response.data);
-            // });
-
-            axios.defaults.xsrfCookieName = 'csrftoken';
-            axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-            axios({
-                method: 'post',
-                url: variables.API_URL+"event",
-                xstfCookieName: 'csrftoken',
-                xsrfHeaderName: 'X-CSRFToken',
-                data: outDict,
-                headers : {
-                    'Content-Type': 'multipart/form-data',
-                    'X-CSRFToken': 'csrftoken',
-                }
-            }).then((response)=>{
-                this.refreshData();
-                alert(response.data);
-            })
-
-
-            //if success
-            //document.getElementById("createButton").disabled = true;
-
-            this.event.skills = before_altered_event.skills
-        },
-        updateClick(){
-            //Store only altered fiels.
-            let before_altered_event = this.getEmptyEvent()
-
-            this.event.skills = this.cleanSkills(this.event.skills);
-            before_altered_event.skills = this.event.skills
-
-            this.event.approved = this.checkboxes.includes('approved')
-            this.event.used_for_calculation = this.checkboxes.includes('used_for_calculation')
-
-            if (!this.user['is_staff']){
-                delete this.event['approved']
-                delete this.event['used_for_calculation']
-            }
-
-            if (this.event.attachment_file == null || typeof this.event.attachment_file === 'string' )
-                delete this.event.attachment_file
-
-
-            this.event.skills = JSON.stringify(this.event.skills)
-            alert(JSON.stringify(this.event, null, 2))
-            let outDict = new FormData();
-            for (const [key, value] of Object.entries(this.event)) {
-                outDict.append(key.toString(), value)
-            }
-
-            //Make a request.
-            axios.defaults.xsrfCookieName = 'csrftoken';
-            axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-            axios({
-                method: 'put',
-                url: variables.API_URL+"event/" + this.event.eventId,
-                xstfCookieName: 'csrftoken',
-                xsrfHeaderName: 'X-CSRFToken',
-                data: outDict,
-                headers : {
-                    'Content-Type': 'multipart/form-data',
-                    'X-CSRFToken': 'csrftoken',
-                }
-            }).then((response)=>{
-                this.refreshData();
-                alert(response.data);
-            })
-
-            //Assign some values back.
-            this.event.skills = before_altered_event.skills
-        },
-        deleteClick(eventId){
-            if(!confirm("Are you sure?")){
-                return;
-            }
-
-            axios.defaults.xsrfCookieName = 'csrftoken';
-            axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-            axios({
-                method: 'delete',
-                url: variables.API_URL+"event/"+ eventId,
-                xstfCookieName: 'csrftoken',
-                xsrfHeaderName: 'X-CSRFToken',
-                headers : {
-                    'X-CSRFToken': 'csrftoken',
-                }
-            }).then((response)=>{
-                this.refreshData();
-                alert(response.data);
-            })
-
-        },
-        addSkillClick(){
-            this.event.skills.push({
-                skillId: '',
-            })
-        },
-        removeSkillClick(){
-            this.event.skills.pop()
-        },
-        cleanSkills(skills){
-            //Remove empty or redundant inputs.
-            nonEmpty = []
-            skillIds = []
-            for (i=0;i<skills.length; ++i) {
-                id = skills[i]['skillId']
-
-                if ( id !== "" && !skillIds.includes(id)){
-                    nonEmpty.push(skills[i]);
-                    skillIds.push(id)
-                }
-            }
-            return nonEmpty
-        },
-
-        getEmptyEvent(){
-
-            let event= {
-                eventId:0,
-                title:"",
-                date:"",
-                mainStaffId:"",
-                info:"",
-                skills: [],
-                approved:false,
-                used_for_calculation: false,
-                attachment_link: "",
-                attachment_file: "",
-            }
-
-            return event
-        },
-
-        onFileSelected(event){
-            this.event.attachment_file = event.target.files[0]
-
-            let label = document.getElementById("attachment_filename");
-            label.innerText = this.event.attachment_file.name
-
-        },
-
         cleanAttachment_file(attachement_file){
 
         },
 
         submitChartDataClick(){
 
-            this.radarChart.destroy()
+            for (let i=0; i<this.radarcharts.length; ++i){
+                this.radarcharts[i].destroy()
+            }
             this.reloadCharts()
 
             let outDict = JSON.parse(JSON.stringify(this.skillTable))
-            console.log(outDict)
-            console.log(this.skillTable)
+
             for (let i = 0; i < this.skillTable.length ;++i){
                 outDict[i].goal_point = this.skillFreq[i]
             }
-            console.log(outDict)
+
 
             axios.defaults.xsrfCookieName = 'csrftoken';
             axios.defaults.xsrfHeaderName = 'X-CSRFToken';
@@ -278,23 +91,57 @@ let chartComponent = {
             })
             //push to DB.
         },
+
+        getEmptyArrayOfArrays(arrayLength){
+            let arr = new Array(arrayLength)
+            for (let i = 0; i < arrayLength; ++i ){
+                arr[i] = []
+            }
+
+            return arr
+        },
         reloadCharts(){
-
-            //Process Data
-            //Handle the case where the ids of skills do not start with 1.
-            let chartContainer = document.getElementById('chart-inputs')
-            if (this.user.is_student)
-                chartContainer.style.visibility = "hidden"
-
-            const ctx = document.getElementById('myChart').getContext('2d');
+             //Prepare chartFreqData for each chart.
+            let n_types = 2
+            this.radarcharts = []
+            let chart_freq_data = this.getEmptyArrayOfArrays(n_types)
+            let goal_freq_data = this.getEmptyArrayOfArrays(n_types)
+            let skill_label_data = this.getEmptyArrayOfArrays(n_types)
 
 
-            const chart_data = {
+            for (let i = 0; i < this.skillTable.length; ++i){
+                for (let j = 0; j < n_types; ++j){
+                    if (this.skillTable[i].type === (j+1)){
+                        chart_freq_data[j].push(this.skillFreq[i])
+                        goal_freq_data[j].push(this.skillTable[i].goal_point)
+                        skill_label_data[j].push(this.skillTable[i].title)
 
-                labels: this.skillLabels,
-                datasets: [{
+                    }
+                }
+            }
+
+            // let chart_input_button = document.getElementById('chart-input-button')
+            //
+            // if (this.user.is_student)
+            //     chart_input_button.style.visibility = "hidden"
+            // console.log(skill_label_data)
+            let chart_ids = ['chart-1', 'chart-2']
+            // let chart_input_ids = ['chart-1-inputs', 'chart-2-inputs']
+
+            for(let i = 0; i < n_types; ++i){
+
+
+                const ctx = document.getElementById(chart_ids[i]).getContext('2d');
+
+                const chart_data = {
+                    labels: skill_label_data[i]
+                }
+
+                if (this.user.is_student){
+                chart_data.datasets = [
+                    {
                     label: 'My Skills',
-                    data: this.skillFreq,
+                    data: chart_freq_data[i],
                     fill: true,
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
                     borderColor: 'rgb(255, 99, 132)',
@@ -302,14 +149,10 @@ let chartComponent = {
                     pointBorderColor: '#fff',
                     pointHoverBackgroundColor: '#fff',
                     pointHoverBorderColor: 'rgb(255, 99, 132)'
-                },
-                ]
-            };
-
-            if (this.user.is_staff){
-                chart_data.datasets[0] = {
+                    },
+                    {
                     label: 'Goal',
-                    data: this.skillFreq,
+                    data: goal_freq_data[i],
                     fill: true,
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgb(54, 162, 235)',
@@ -317,19 +160,14 @@ let chartComponent = {
                     pointBorderColor: '#fff',
                     pointHoverBackgroundColor: '#fff',
                     pointHoverBorderColor: 'rgb(54, 162, 235)'
-                }
-            }
-
-            if (this.user.is_student){
-
-                let goalFreq = []
-                for (let i=0; i < this.skillTable.length; ++i){
-                    goalFreq.push(this.skillTable[i].goal_point)
+                    },
+                    ]
                 }
 
-                goalData = {
+            else if (this.user.is_staff){
+                chart_data.datasets = [{
                     label: 'Goal',
-                    data: goalFreq,
+                    data: chart_freq_data[i],
                     fill: true,
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgb(54, 162, 235)',
@@ -337,12 +175,8 @@ let chartComponent = {
                     pointBorderColor: '#fff',
                     pointHoverBackgroundColor: '#fff',
                     pointHoverBorderColor: 'rgb(54, 162, 235)'
-              }
-
-                chart_data.datasets.push(goalData)
-
+                }]
             }
-
 
             const config = {
                 type: 'radar',
@@ -364,7 +198,9 @@ let chartComponent = {
                 }
               },
             };
-            this.radarChart = new Chart(ctx, config);
+                this.radarcharts.push(new Chart(ctx, config))
+            }
+
         },
 
         prepareChartData(){
@@ -416,12 +252,9 @@ let chartComponent = {
             this.user['is_staff'] = Object.values(this.user.groups).includes('staff')
             this.user['is_student'] = Object.values(this.user.groups).includes('student')
 
-            //Prepare chartData depends on the group of the user.
             this.prepareChartData()
 
-            // console.log(typeof this.user.groups)
 
-            // console.log(this.user)
         },
         async makeRequests(){
             await axios.get(variables.API_URL+"skillTable")
